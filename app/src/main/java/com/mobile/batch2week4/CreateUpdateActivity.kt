a444bf2ddbdd1ac33adc632b1e98e26299e3405f
KMalif <khafidulmualif2019@gmail.com>
2021-04-30 16:24:50 +0700

    2. add function create and function Update

--- app/src/main/java/com/mobile/batch2week4/CreateUpdateActivity.kt
+++ app/src/main/java/com/mobile/batch2week4/CreateUpdateActivity.kt
@@ -2,7 +2,13 @@ package com.mobile.batch2week4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.mobile.batch2week4.databinding.ActivityCreateUpdateBinding
+import com.mobile.batch2week4.model.Posts
+import com.mobile.batch2week4.util.APIClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CreateUpdateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateUpdateBinding
@@ -10,19 +16,73 @@ class CreateUpdateActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateUpdateBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        btnSaveAction()
+        viewDatatoForm()
+        btnUpdateAction()
     }
 
     //Function ketika button save diclick
-
+    private fun btnSaveAction(){
+        binding.BtnSave.setOnClickListener {
+            addPost()
+            finish()
+        }
+    }
 
     //Function untuk menambahkan data ke API
+    private fun addPost(){
+        val title = binding.ETTitle.text.toString()
+        val body = binding.EtBody.text.toString()
+
+        APIClient.APIEndpoint().addPosts(title, body).enqueue(object : Callback<Posts>{
+            override fun onFailure(call: Call<Posts>, t: Throwable) {
+                println(t.message)
+            }
+
+            override fun onResponse(call: Call<Posts>, response: Response<Posts>) {
+                if (response.isSuccessful){
+                    val body = response.body()
+                    Toast.makeText(applicationContext, "Succes create data ", Toast.LENGTH_LONG).show()
+                    println("Data Created $body")
+                }
+            }
+        })
+    }
 
 
     //function ketika button update diclick
-
+    private fun btnUpdateAction(){
+        binding.BtnUpdate.setOnClickListener {
+            updatePost()
+            finish()
+        }
+    }
 
     //function untuk merubah data yang sudah ada dalam API
+    private fun updatePost(){
+        val title = binding.ETTitle.text.toString()
+        val body = binding.EtBody.text.toString()
+        val id = intent.getIntExtra("id", 0)
+        APIClient.APIEndpoint().updatePost(id, title, body).enqueue(object : Callback<Posts>{
+            override fun onFailure(call: Call<Posts>, t: Throwable) {
+                println(t.message)
+            }
+
+            override fun onResponse(call: Call<Posts>, response: Response<Posts>) {
+                if (response.isSuccessful){
+                    val body = response.body()
+                    Toast.makeText(applicationContext, "Data Berhasil diupdate", Toast.LENGTH_LONG).show()
+                    println("Data Baru $body")
+                }
+            }
+        })
+    }
+
+    //function untuk get data dari intent ke form
+    private fun viewDatatoForm(){
+        binding.ETTitle.setText(intent.getStringExtra("title"))
+        binding.EtBody.setText(intent.getStringExtra("Body"))
+    }
 
 
 }
--- app/src/main/java/com/mobile/batch2week4/util/APIEndpoint.kt
+++ app/src/main/java/com/mobile/batch2week4/util/APIEndpoint.kt
@@ -2,9 +2,7 @@ package com.mobile.batch2week4.util
 
 import com.mobile.batch2week4.model.Posts
 import retrofit2.Call
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.*
 
 interface APIEndpoint {
 
@@ -17,7 +15,20 @@ interface APIEndpoint {
     @DELETE("posts/{id}")
     fun delete(@Path("id")id : Int):Call<Void>
 
+    @FormUrlEncoded
+    @POST("posts")
+    fun addPosts(
+            @Field("title") title : String,
+            @Field("body") body : String
+    ): Call<Posts>
 
+    @FormUrlEncoded
+    @POST("posts/{id}")
+    fun updatePost(
+            @Path("id") id : Int,
+            @Field("title") title : String,
+            @Field("body") body : String
+    ): Call<Posts>
 
 }
 
